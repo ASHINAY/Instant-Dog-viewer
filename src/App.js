29d43f1408e2acb0c7a288e570bc32257902fb82
Nayak <anuja.nayak@capgemini.com>
2023-09-24 15:18:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useState} from "react";
 
 function App() {
+  const[img,setImg] = useState("")
+  const[btndisabled,setBtnDisabled] = useState(false);
+  const onBtnClick = () =>{
+    const url = "https://random.dog/woof.json";
+    axios
+    .get(url)
+    .then((response)=>{
+      let imgUrl = response.data.url
+      let lastThreeLetter = imgUrl.slice(-3);
+      if(lastThreeLetter==="mp4"){
+          onBtnClick()
+      }else{
+      setImg(imgUrl);
+      setBtnDisabled(false);
+    }
+      console.log(lastThreeLetter);
+    })
+    .catch((error)=>{
+      console.log("Error:",error)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Instant Dog Viewer</h1>
+      <button onClick={onBtnClick} disabled={btndisabled}>Show me a Dog</button>
+      {img !== "" ?  (<img className='img' src={img}/>) : null}
     </div>
   );
 }
